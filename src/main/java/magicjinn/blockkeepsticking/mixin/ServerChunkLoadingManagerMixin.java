9755fc887bf028f30c3f4a18b43b60e2479696e5
MagicJinn <jinn.bz@mail.com>
2025-10-12 22:12:39 +0200

    Possibly fixed ServerChunkLoadingManager (previously anvil something) mixin

--- src/main/java/magicjinn/blockkeepsticking/mixin/ServerChunkLoadingManagerMixin.java
+++ src/main/java/magicjinn/blockkeepsticking/mixin/ServerChunkLoadingManagerMixin.java
@@ -1,22 +1,22 @@
 package magicjinn.blockkeepsticking.mixin;
 
 import magicjinn.blockkeepsticking.manager.ChunkManager;
-import net.minecraft.server.world.ThreadedAnvilChunkStorage;
+import net.minecraft.server.world.ServerChunkLoadingManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ThreadedAnvilChunkStorage.class)
-public class ThreadedAnvilChunkStorageMixin {
+@Mixin(ServerChunkLoadingManager.class)
+public class ServerChunkLoadingManagerMixin {
 	
 	@Inject(method = "save(Z)V", at = @At("HEAD"))
 	private void onSave(boolean flush, CallbackInfo ci) {
 		if (flush) {
 			@SuppressWarnings({ "resource", "unused" })
-			ThreadedAnvilChunkStorage storage = (ThreadedAnvilChunkStorage)(Object)this;
-			// Como ya no tenemos acceso directo al chunk, el ChunkManager se encargará
-			// de procesar los chunks pendientes
+			ServerChunkLoadingManager storage = (ServerChunkLoadingManager) (Object) this;
+			// Since we no longer have direct access to the chunk, the ChunkManager will
+			// take care of processing the pending chunks
 			ChunkManager.getInstance().processAllPendingChunks();
 		}
 	}
