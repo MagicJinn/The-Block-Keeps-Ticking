051c62962f5c93af9ea687fd67e427dabeb95d25
Marijn Bogerd <72989508+MagicJinn@users.noreply.github.com>
2025-10-24 00:43:04 +0200

    Update ProcessingBlock.java

--- src/main/java/magicjinn/theblockkeepsticking/framework/ProcessingBlock.java
+++ src/main/java/magicjinn/theblockkeepsticking/framework/ProcessingBlock.java
@@ -1,7 +1,7 @@
+//TODO: Remove!
 package magicjinn.theblockkeepsticking.framework;
 
 import net.minecraft.block.entity.BlockEntity;
-//TODO: Remove!
 // Things like furnaces, that process items over time
 public abstract class ProcessingBlock extends TickingBlock {
     public abstract void Simulate(BlockEntity blockInstance, Long ticksToSimulate);
