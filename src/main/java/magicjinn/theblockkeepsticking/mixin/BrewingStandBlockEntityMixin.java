78a9c86e2bb1507cf334bcff4faafd186a219fe6
MagicJinn <jinn.bz@mail.com>
2025-10-27 21:55:52 +0100

    brewing stand.

--- src/main/java/magicjinn/theblockkeepsticking/mixin/BrewingStandBlockEntityMixin.java
+++ src/main/java/magicjinn/theblockkeepsticking/mixin/BrewingStandBlockEntityMixin.java
@@ -1,6 +1,7 @@
 package magicjinn.theblockkeepsticking.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BrewingStandBlockEntity;
 import net.minecraft.util.math.BlockPos;
@@ -9,8 +10,20 @@ import magicjinn.theblockkeepsticking.util.TickingBlockAccessor;
 
 @Mixin(BrewingStandBlockEntity.class)
 public class BrewingStandBlockEntityMixin implements TickingBlockAccessor {
+    @Shadow private int brewTime;
+
+    static final int MAX_BREWING_TIME = 400;
+
     @Override
     public boolean Simulate(Long ticksToSimulate, World world, BlockState state, BlockPos pos) {
+        // Since a brewing stand can only process 1 item at a time, and we don't support hoppers,
+        // we only need to check whether there is already a recipe in progress,
+        // and subtract time from it
+
+        if (brewTime < MAX_BREWING_TIME && brewTime > 0) { // This means we're brewing
+            brewTime = Math.max(20, brewTime - ticksToSimulate.intValue());
+            return true;
+        }
         return false;
     }
 }
